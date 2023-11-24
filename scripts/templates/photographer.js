2bc479a2b1e9d3f60c4a4c709f92f80f56ca0a4c
Cristel <stelda@outlook.fr>
2023-11-24 10:42:47 +0100

    Extend protographerTemplate function
    Add city, country, tagline and price information

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,25 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const location = document.createElement( 'p' );
+        location.textContent = `${city}, ${country}`;
+        location.classList.add( 'location' );
+
+        const taglineElement = document.createElement( 'p' );
+        taglineElement.textContent = tagline;
+        taglineElement.classList.add( 'tagline' );
+
+        const priceElement = document.createElement( 'p' );
+        priceElement.textContent = `${price}€/jour`;
+        priceElement.classList.add( 'price' );
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(taglineElement);
+        article.appendChild(priceElement);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
