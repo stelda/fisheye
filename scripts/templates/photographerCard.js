003774d203609d99d78fe343959479409f37152e
Cristel <stelda@outlook.fr>
2023-11-27 10:36:04 +0100

    Refactored Photographer Card Structure with Paragraph Function

--- scripts/templates/photographerCard.js
+++ scripts/templates/photographerCard.js
@@ -1,3 +1,4 @@
+/* this script is used to display photographers data on index.html page */
 function photographerTemplate(data) {
     const { name, id, portrait, city, country, tagline, price } = data;
 
@@ -8,26 +9,25 @@ function photographerTemplate(data) {
 
         // Create link to photographer page
         const link = document.createElement( 'a' );
-        link.setAttribute("href", `../../photographer.html?id=${id}`);
+        link.setAttribute("href", `photographer.html?id=${id}`);
         link.classList.add( 'photographer-card' );
 
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
 
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
 
-        const location = document.createElement( 'p' );
-        location.textContent = `${city}, ${country}`;
-        location.classList.add( 'location' );
+        function createParagraph(text, className) {
+            const paragraph = document.createElement( 'p' );
+            paragraph.textContent = text;
+            paragraph.classList.add( className );
+            return paragraph;
+        }
 
-        const taglineElement = document.createElement( 'p' );
-        taglineElement.textContent = tagline;
-        taglineElement.classList.add( 'tagline' );
-
-        const priceElement = document.createElement( 'p' );
-        priceElement.textContent = `${price}€/jour`;
-        priceElement.classList.add( 'price' );
+        const location = createParagraph(`${city}, ${country}`, 'location');
+        const taglineElement = createParagraph(tagline, 'tagline');
+        const priceElement = createParagraph(`${price}€/jour`, 'price');
 
         article.appendChild(link);
         link.appendChild(img);
