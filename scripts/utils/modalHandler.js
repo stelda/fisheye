16bb1d92556c6ac3cb7084a9b416f077f9b5b859
Cristel <stelda@outlook.fr>
2023-12-22 09:53:19 +0100

    Close contact modal with keyboard

--- scripts/utils/modalHandler.js
+++ scripts/utils/modalHandler.js
@@ -1,7 +1,12 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
-}
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            closeModal();
+        }
+    }
+    )};
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
