c7189ffc633dd407b218d04699e1a7e88ccc1a36
Cristel <stelda@outlook.fr>
2023-12-28 09:15:52 +0100

    Remove debugging console.log

--- scripts/templates/photographerDetails.js
+++ scripts/templates/photographerDetails.js
@@ -60,7 +60,6 @@ export function displayPhotographerPortfolio(selectedPortfolio) {
                 const video = mediaFactory.createVideoElement(media.video, media.title, media.likes);
                 portfolio.appendChild(video);
             }
-            console.log("date" + media.date + "titre :" + media.title + "likes :" + media.likes); // for debugging
         });
     }
 
